36777239c28ba520314f1dde246ed6d71100df9f
mathletedev <mathletedev@gmail.com>
2021-11-06 18:15:55 -0700

    remove p and c aliases

--- src/functions.ts
+++ src/functions.ts
@@ -1,8 +1,6 @@
 import { combinations, ImportObject, permutations } from "mathjs";
 
 export const functions: ImportObject = {
-	perm: permutations,
-	comb: combinations,
 	bino: (n: number, k: number, p: number) =>
 		combinations(n, k) * Math.pow(p, n) * Math.pow(1 - p, n - k),
 	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k)
