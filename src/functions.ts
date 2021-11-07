6b470e3f132a5f5243f5e4cdf688383d87f1ea2d
mathletedev <mathletedev@gmail.com>
2021-11-06 18:12:16 -0700

    just create aliases for p and c

--- src/functions.ts
+++ src/functions.ts
@@ -1,8 +1,8 @@
-import { BigNumber, combinations, ImportObject, permutations } from "mathjs";
+import { combinations, ImportObject, permutations } from "mathjs";
 
 export const functions: ImportObject = {
-	perm: (n: BigNumber, k: BigNumber) => permutations(n, k),
-	comb: (n: BigNumber, k: BigNumber) => combinations(n, k),
+	perm: permutations,
+	comb: combinations,
 	bino: (n: number, k: number, p: number) =>
 		combinations(n, k) * Math.pow(p, n) * Math.pow(1 - p, n - k),
 	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k)
