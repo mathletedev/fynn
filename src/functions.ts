a32c0721f02d8fcf27055d1475061db4a18fa8a8
mathletedev <mathletedev@gmail.com>
2021-11-07 21:21:59 -0800

    replace n with k

--- src/functions.ts
+++ src/functions.ts
@@ -3,7 +3,8 @@ import { combinations, ImportObject, permutations } from "mathjs";
 export const functions: ImportObject = {
 	p: permutations,
 	c: combinations,
-	bn: (n: number, k: number, p: number) =>
-		combinations(n, k) * Math.pow(p, n) * Math.pow(1 - p, n - k),
+	bn: (n: number, k: number, p: number) => {
+		return combinations(n, k) * Math.pow(p, k) * Math.pow(1 - p, n - k);
+	},
 	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k)
 };
