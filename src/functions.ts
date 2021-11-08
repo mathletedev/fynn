653b0caee39d09ab9879c75abfdd14812ee3884d
mathletedev <mathletedev@gmail.com>
2021-11-07 20:36:57 -0800

    aliases for p c and bino

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,9 @@
 import { combinations, ImportObject, permutations } from "mathjs";
 
 export const functions: ImportObject = {
-	bino: (n: number, k: number, p: number) =>
+	p: permutations,
+	c: combinations,
+	bn: (n: number, k: number, p: number) =>
 		combinations(n, k) * Math.pow(p, n) * Math.pow(1 - p, n - k),
 	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k)
 };
