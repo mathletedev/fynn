eefdeb1c1c06f3f51ddf94e54b84d9765925bac7
Delta Eames <mathletedev@gmail.com>
2022-04-17 12:26:45 -0700

    z-score funciton

--- src/functions.ts
+++ src/functions.ts
@@ -6,5 +6,29 @@ export const functions: ImportObject = {
 	bn: (n: number, k: number, p: number) => {
 		return combinations(n, k) * Math.pow(p, k) * Math.pow(1 - p, n - k);
 	},
-	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k)
+	bp: (n: number, k: number) => permutations(n, k) / Math.pow(n, k),
+	z: (z: number) => {
+		if (z < -6.5) return 0.0;
+		if (z > 6.5) return 1.0;
+
+		let f = 1;
+		let s = 0;
+		let t = 1;
+		let k = 0;
+		let l = Math.exp(-23);
+
+		while (Math.abs(t) > l) {
+			t =
+				(((0.3989422804 * Math.pow(-1, k) * Math.pow(z, k)) /
+					(2 * k + 1) /
+					Math.pow(2, k)) *
+					Math.pow(z, k + 1)) /
+				f;
+			s += t;
+			k++;
+			f *= k;
+		}
+
+		return s + 0.5;
+	}
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv-safe/config";
 import { Master } from "eris-sharder";
 
-const _ = new Master(process.env.BOT_TOKEN!, "/dist/main.js", {
+new Master(process.env.BOT_TOKEN!, "/dist/main.js", {
 	stats: true,
 	name: "Fynn",
 	clientOptions: {}
